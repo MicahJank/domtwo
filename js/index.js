8b0796ff2967e0aa6e7f35ab7627bbe16dba6e14
MicahJank <MicahJank@users.noreply.github.com>
2019-06-06 23:19:18 -0500

    finished making one event listener for the navlinks

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// assign elements to variables
+const navLinks = document.querySelectorAll('.nav a');
+
+// Event Listeners
+
+// This does something similar to the :hover effect in css
+navLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.classList.add('navLinkColor');
+    });
+
+    link.addEventListener('mouseleave', () => {
+        link.classList.remove('navLinkColor');
+    });
+});
